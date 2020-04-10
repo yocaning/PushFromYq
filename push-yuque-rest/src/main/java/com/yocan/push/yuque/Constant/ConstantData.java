22d52d8a3f6f0f0aa1b9a72d1a71c69b491afa77
yocan <1091270264@qq.com>
2020-04-10 15:25:55 +0800

    feat:update push url list

--- push-yuque-rest/src/main/java/com/yocan/push/yuque/Constant/ConstantData.java
+++ push-yuque-rest/src/main/java/com/yocan/push/yuque/Constant/ConstantData.java
@@ -5,7 +5,16 @@ import java.util.List;
 
 public class ConstantData {
 
-    public static final List<String> URL = Arrays.asList("https://sc.ftqq.com/SCU40692Tccb6b97f9f07146f0b5ac909ca1bfcf35c3c6e3cd17d2.send","https://sc.ftqq.com/SCU91391T98d6e181aadd26097d34bd9c4817626a5e7daf64673f5.send");
+    public static final List<String> URL = Arrays.asList(
+            "https://sc.ftqq.com/SCU40692Tccb6b97f9f07146f0b5ac909ca1bfcf35c3c6e3cd17d2.send",
+            "https://sc.ftqq.com/SCU91391T98d6e181aadd26097d34bd9c4817626a5e7daf64673f5.send",
+            "https://sc.ftqq.com/SCU93338T1744c51eeeb7d5b8774cfd6ed039c0345e900faed9559.send",
+            "https://sc.ftqq.com/SCU93340T07faef67160c9a92c5386c2fd8844b3d5e9012e795322.send",
+            "https://sc.ftqq.com/SCU93341T73a7b6396c4f40fd2b1bc5f28935c7e25e9011a431bd3.send",
+            "https://sc.ftqq.com/SCU93344T8e566b262a0fd07ae03a0220e55453e75e9017597f575.send",
+            "https://sc.ftqq.com/SCU93348T8b85b4d49cf48ec6f6fcfb17b85df1d65e90182c2b6ee.send",
+            "https://sc.ftqq.com/SCU93347Te1965e405e92f70bc10c970b4785100d5e9017d805056.send"
+    );
 
 
 
